3a08ee96b55c5e28c84ca21a023084104535c6b2
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-12-20 23:41:14 -0800

    refactor form names

--- file_upload_python/app_one/views.py
+++ file_upload_python/app_one/views.py
@@ -6,25 +6,25 @@ from .forms import *
 def hotel_image_view(request): 
   
     if request.method == 'POST': 
-        form2 = HotelForm(request.POST, request.FILES) 
-        form = ImgForm(request.POST, request.FILES) 
+        hotelForm= HotelForm(request.POST, request.FILES) 
+        imageForm= ImgForm(request.POST, request.FILES) 
        
   
-        if form2.is_valid() and form.is_valid(): 
+        if hotelForm.is_valid() and imageForm.is_valid(): 
             if request.POST['name'] != "":
                 print(request.POST['name'])
-                form2.save()
+                hotelForm.save()
                 newHotel = Hotel.objects.order_by('-id')[0]
                 newImage = Image.objects.create(hotel_Main_Img = request.FILES['hotel_Main_Img'], hotel = newHotel)
                 newImage.save()
             else:
-                form.save()
+                imageForm.save()
 
             return redirect('success') 
     else: 
-        form2 = HotelForm() 
-        form = ImgForm() 
-    return render(request, 'hotel_image_form.html', {'form2' : form2, 'form' : form, }) 
+        hotelForm = HotelForm() 
+        imageForm = ImgForm() 
+    return render(request, 'hotel_image_form.html', {'hotelForm' : hotelForm, 'imageForm' : imageForm }) 
   
   
 def success(request): 
